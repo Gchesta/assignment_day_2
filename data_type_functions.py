1ebda54f8e6c1bb7c238edaf3dac0ec5e039a6be
Governor <elimushwari@gmail.com>
2017-02-08 20:13:46 +0300

    data type

--- data_type_functions.py
+++ data_type_functions.py
@@ -1,32 +1,32 @@
-def data_type(some_data):
-  try:
-    if isinstance(some_data, str):
-      return len(some_data)
+
+def data_type(some_data=None):
   
-    elif isinstance(some_data, bool):
-      return some_data
+  if some_data == None:
+    return "no value"
+  
+  elif isinstance(some_data, str):
+    return len(some_data)
+  
+  elif isinstance(some_data, bool):
+    return some_data
     
-    elif isinstance(some_data, int):
+  elif isinstance(some_data, int):
       
-      if some_data < 100:
-        return "less than 100"
+    if some_data < 100:
+      return "less than 100"
     
-      elif some_data == 100:
-        return "equal to 100"
+    elif some_data == 100:
+      return "equal to 100"
     
-      else:
-        return "more than 100"
+    else:
+      return "more than 100"
       
-    elif isinstance(some_data, list):
-      if len(some_data) >= 3:
-        return some_data[2]
+  elif isinstance(some_data, list):
+    if len(some_data) >= 3:
+      return some_data[2]
     
     else:
       return None
-    
-  except TypeError: #raises an exception when no value is submitted
-    return "No Value"
-  
-  
-  
       
+  
+    
