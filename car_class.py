f0ff802f77a4461ac80b508c86feea3a17d3dc6b
Governor <elimushwari@gmail.com>
2017-02-08 22:28:02 +0300

    Final code

--- car_class.py
+++ car_class.py
@@ -1,42 +1,42 @@
 
 class Car(object):
-  """This car class can be used to instantiate various vehicles. Each car has got different attributes:
-  name: eg Toyota, Honda
-  model: eg Corrola
-  type: eg truck, trailer"""
   
-  
-  def __init__(self, name, model, car_type):
-    #a function to initialize the Car class
-    
+  def __init__(self, name="General", model="GM", car_type="saloon"):
+    #The car should be called `General` if no name was passed as an argument
+    #The car's model should be called `GM` if no model was passed as an argument
+    #The car type should be saloon if it is not a trailer
+    self.name = name
     self.model = model
+    self.car_type = car_type
+    self.speed = 0 
     
-    #estblish a saloon car as the default non-trailer car
-    if car_type== "Trailer":
-      self.car_type = car_type
-    else:
-      self.car_type = "Saloon"
-    
-   
-   #establishing the default name "General" 
-    if name == None:
-      self.name = "General"
-    else:
-      self.name = name
-    
-    #establishing conditions for the number of doors  
-    if name == "Koenigsegg" or name == "Agera R"":
-      self.num_of_doors = 4
-    else:
+    #The car shoud have four (4) doors except its a Porshe or Koenigsegg
+    if self.name == "Porshe" or self.name == "Koenigsegg":
       self.num_of_doors = 2
-      
-    #establishing conditions for the number of wheels  
-    if car_type == "Trailer":
+    
+    else:
+      self.num_of_doors = 4
+    
+    #The car shoud have four (4) wheels except its a type of trailer
+    if self.car_type == "trailer":
       self.num_of_wheels = 8
+  
     else:
       self.num_of_wheels = 4
-
-
-      
-      
   
+  #The car type should be saloon if it is not a trailer
+  def is_saloon(self):
+    if self.car_type == "saloon":
+      return True
+    return False
+    
+  def drive(self, factor):
+    #The Trailer should have speed 0 km/h until you put `the pedal to the metal
+    if self.name == "Mercedes":
+      self.speed = 10 ** factor
+    
+    #The Mercedes should have speed 0 km/h until you put `the pedal to the metal  
+    elif self.car_type == "trailer":
+      self.speed = 11 * factor
+      
+    return self
