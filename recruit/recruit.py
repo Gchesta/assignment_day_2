2b530487f662cc5443d085d25129c72220b8c521
Governor <elimushwari@gmail.com>
2017-02-12 06:17:43 +0300

    Initialize the four sub-classes of the Recruit superclass

--- recruit/recruit.py
+++ recruit/recruit.py
@@ -7,21 +7,84 @@ class Recruit(object)
 	
 	def __init__(self, full_name, age, national_id, military_id):
 		"""A function to initialize the new recruit"""
-		
 		self.full_name = full_name #full_name of recruit
 		self.age = age #age f recruit
 		self.national_id = national_id #recruits national id
-		
-	
-	def __hiv_scholarship(self, amount):
-		"""setting a private method to make use of encapsulation"""
-		self.amount = amount
-		
- class Prefect(Student):
-	"""setting up a new prefect - employing inheritance"""
-	def __init__(self, admin_num, name, kcpe_marks, age, hostel, stream, former_school, reward_points=0, rank):
-		Student.__init__(self, admin_num, name, kcpe_marks, age, hostel, stream, former_school, reward_points=0 )
-		self.rank = rank
-		
-		
-	
+		self.military_id = military_id
+
+		#checking validity of enries
+		if not str(full_name).isalpha():
+			raise TypeError("Names MUST contain ONLY alphabetic charaters")
+
+		if not str(age).isdigit():
+			raise TypeError("The age MUST contain ONLY characters 0 - 9")
+
+		if not str(national_id).isdigit():
+			raise TypeError("The national ID MUST contain ONLY characters 0 - 9")
+
+		if not str(military_id).isdigit():
+			raise TypeError("The military ID MUST contain ONLY characters 0 - 9")
+
+		if len(str(military_id)) != 8 or len(str(national_id)) != 8
+			raise ValueError("zThe length of the national/military ID MUST be 8 digits")
+
+		if self.age < 18:
+			raise ValueError("A recruit cannot be less than 18 years old")
+				
+class ServiceMan(Recruit):
+	"""setting up a new ServiceMan - employing inheritance"""
+	def __init__(self, full_name, age, national_id, military_id, kcse_points):
+		Recruit.__init__(self, full_name, age, national_id, military_id)
+
+		self.grade = kcse_points
+
+		if self.age > 22:
+			raise ValueError("A serviceman cannot be more than 22 years old")
+
+		if self.grades < 25:
+			raise ValueError("A serviceman should have scored at least 25 points in the KCSE (D+ and above)")
+
+
+
+class GsoCadet(Recruit):
+	"""setting up a new General Service Officer Cadet - employing inheritance"""
+	def __init__(self, full_name, age, national_id, military_id, kcse_points):
+		Recruit.__init__(self, full_name, age, national_id, military_id)
+
+		self.grade = kcse_points
+
+		if self.age > 26:
+			raise ValueError("A General Service Officer Cadet cannot be more than 26 years old")
+
+		if self.grades < 25:
+			raise ValueError("A General Service Officer Cadet should have scored at least 25 points in the KCSE (D+ and above)")
+
+class SoCadet(Recruit):
+	"""setting up a new Specialist Officer Cadet - employing inheritance"""
+	def __init__(self, full_name, age, national_id, military_id, profession):
+		Recruit.__init__(self, full_name, age, national_id, military_id)
+		self.profession = profession
+		needed_professions = ["Doctor", "Nurse", "Chaplain", "Clinical Officer", "Engineer"]
+
+		if self.age > 29:
+			raise ValueError("A Specialist Officer Cadet cannot be more than 26 years old")
+
+		if self.profession not in needed_professions:
+			raise ValueError("Too bad. We currently don't have any need for " + self.profession)
+
+class Constabulary(Recruit):
+	"""setting up a new Specialist Officer Cadet - employing inheritance"""
+	def __init__(self, full_name, age, national_id, military_id, experince):
+		Recruit.__init__(self, full_name, age, national_id, military_id)
+
+		self.experience = experience
+
+		if self.age < 30 or self.age > 49:
+			raise ValueError("A constabulary must be between 30 and 49 years of age")
+
+
+
+			
+
+
+
